663327b77b4ba28c3cb6e9ff6fe7b48d10527076
stbchen <stbchen@ucsc.com>
2022-05-15 16:22:32 -0700

    block

--- src/Play.js
+++ src/Play.js
@@ -6,6 +6,7 @@ class Play extends Phaser.Scene {
   preload() {
     this.load.image('player_sprite', './assets/player.png');
     this.load.image('background_img', './assets/background.png');
+    this.load.image('block_img', './assets/block.png');
   }
 
   create() {
@@ -13,14 +14,20 @@ class Play extends Phaser.Scene {
     this.add.tileSprite(0, 0, game.config.width, game.config.height, 'background_img').setOrigin(0, 0);
 
     // Create the player sprite
-    this.player = this.physics.add.sprite(0, 0, 'player_sprite');
+    this.player = this.physics.add.sprite(0, 0, 'player_sprite').setOrigin(0, 0);
     this.player.setCollideWorldBounds(true);
 
-    // Adding keyboard controls
-    keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-    keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-    keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-    keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    // Create the block sprite
+    this.block = this.physics.add.image(32, 32, 'block_img').setOrigin(0, 0);
+    this.block.setCollideWorldBounds(true);
+    this.block.setBounce(1);
+    this.block.setVelocity(-200, 60);
+
+
+    this.playerBlockCollison();
+
+    // Add controls
+    this.addControls();
 
   }
 
@@ -83,4 +90,15 @@ class Play extends Phaser.Scene {
 
   }
 
+  addControls() {
+    // Adding keyboard controls
+    keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+    keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+  }
+
+  playerBlockCollison() {
+    this.physics.add.collider(this.player, this.block);
+  }
 }
